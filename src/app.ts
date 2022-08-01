c0980c7dfc46a11c0e034b656bce509121781ca0
hamza-che <chenguitiha@gmail.com>
2022-08-01 11:52:32 +0100

    add project list class

--- src/app.ts
+++ src/app.ts
@@ -61,6 +61,42 @@ function Autobind(
   return adjDescriptor;
 }
 
+// ProjectList class
+class PropjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "finished" | "active") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector(
+      "h2"
+    )!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+  }
+
+  attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 // ProjectInput class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -162,3 +198,5 @@ class ProjectInput {
 }
 
 const prjctInput = new ProjectInput();
+const activeProjects = new PropjectList("active");
+const finishedProjects = new PropjectList("finished");
